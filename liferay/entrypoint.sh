73acb19995530dfcd4a7a56591dc1cecf0a40a46
ryanschuhler <ryan.schuhler@liferay.com>
2017-11-28 17:09:19 -0800

    Deploying from git workspace

--- liferay/entrypoint.sh
+++ liferay/entrypoint.sh
@@ -8,7 +8,8 @@ main() {
   prepare_liferay_tomcat_config
   prepare_liferay_deploy_directory
   prepare_liferay_osgi_configs_directory
-  run_portal "$@"
+  prepare_liferay_workspace
+  exec "$@"
 }
 
 show_motd() {
@@ -114,27 +115,38 @@ prepare_liferay_tomcat_config() {
   "
 }
 
-run_portal() {
-
-  set -e
-
-  # Drop root privileges if we are running liferay
-  # allow the container to be started with `--user`
-  if [ "$1" = 'catalina.sh' -a "$(id -u)" = '0' ]; then
-    # Change the ownership of Liferay Shared Volume to liferay
-
-    if [[ ! -d "$LIFERAY_SHARED" ]]; then
-      mkdir -p $LIFERAY_SHARED
-    fi
-
-    chown -R liferay:liferay $LIFERAY_SHARED
-    chown -R liferay:liferay $LIFERAY_HOME
+prepare_liferay_workspace() {
+  if [ ! -f $WORKSPACE_DIR/* ]; then
+    echo "No modules found.
+  If you wish to deploy customizations to Liferay through
+  your workspace make sure your WORKSPACE_URL is correct
+  and there are no build errors.
+  Continuing.
+  "
+    return 0
   fi
 
-  # As argument is not related to liferay,
-  # then assume that user wants to run his own process,
-  # for example a `bash` shell to explore this image
-  exec "$@"
+  echo "Modules directory found.
+  Running gradle deploy.
+  "
+
+  set -x
+
+  cd $WORKSPACE_DIR
+  ./gradlew deploy
+
+  set +x
+
+  echo "The following contents are going
+  to be synchronized with Liferay:
+  "
+
+  tree $WORKSPACE_DIR/bundles/osgi/modules
+
+  [ -f $WORKSPACE_DIR/bundles/osgi/modules/*.jar ] && cp $WORKSPACE_DIR/bundles/osgi/modules/*.jar $LIFERAY_HOME/deploy
+  echo "
+  Continuing.
+  "
 }
 
 main "$@"
